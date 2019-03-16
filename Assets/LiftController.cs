93923aa7d2f45cb145f7f4e86b8d40c021fd87a7
Gusarov Vladislav <programm00@ya.ru>
2019-03-16 15:29:02 +1000

    made lift

---
+++ Assets/LiftController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftController : MonoBehaviour
+{
+    public float dy = 0.05f;
+    
+    void Start()
+    {
+        
+    }
+    
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y + dy, transform.position.z);
+        transform.position = newPosition;
+        if (transform.position.y > 30.0 || transform.position.y < 0.0) 
+        {
+            dy = -dy;
+        }
+    }
+}
